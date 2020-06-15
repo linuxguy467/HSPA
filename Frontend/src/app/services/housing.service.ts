1ce2442ea235df427ebbdc079f315d48b87181c9
Matthew Hemingway <dpplrdr435@gmail.com>
2020-06-15 16:02:18 -0400

    Added IProperty interface for type checking. Modified http calls to use RxJS operators.

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { IProperty } from '../property/IProperty.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,16 @@ export class HousingService {
 
   constructor(private http: HttpClient) { }
 
-  getAllProperties() {
-    return this.http.get('data/properties.json')
+  getAllProperties(): Observable<IProperty[]> {
+    return this.http.get('data/properties.json').pipe(
+      map(data => {
+        const propertiesArray: Array<IProperty> = [];
+        for(const i in data) {
+          if(data.hasOwnProperty(i))
+            propertiesArray.push(data[i]);
+        }
+        return propertiesArray;
+      })
+    );
   }
 }
