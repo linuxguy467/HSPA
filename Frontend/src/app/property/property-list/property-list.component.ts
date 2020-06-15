484cf73a723ba1949c6c481bb8b561482244beb3
Matthew Hemingway <dpplrdr435@gmail.com>
2020-06-15 15:38:22 -0400

    Added Housing Service. Moved all property code to properties.json and calls to HousingService.

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from '../../services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -6,48 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any> = [
-    {
-      "id": 1,
-      "name": "Birla House",
-      'type': 'House',
-      'price': 12000
-    },
-    {
-      "id": 2,
-      "name": "Erose Villa",
-      'type': 'Villa',
-      'price': 14000
-    },
-    {
-      "id": 3,
-      "name": "Mark Hill",
-      'type': 'Flat',
-      'price': 12500
-    },
-    {
-      "id": 4,
-      "name": "Gun Hill",
-      'type': 'Duplex',
-      'price': 11400
-    },
-    {
-      "id": 5,
-      "name": "Macro House",
-      'type': 'House',
-      'price': 14300
-    },
-    {
-      "id": 6,
-      "name": "Pearl White House",
-      'type': 'House',
-      'price': 18000
-    }
-  ]
+  properties: any;
 
-  constructor() { }
+  constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data => this.properties = data, error => console.error(error)
+    );
   }
 
 }
